7c0d4722e231c57ec16f159670cfec49908fa36b
Jan Fedorowicz <janek.fedorowicz@gmail.com>
2021-07-11 11:56:07 +0200

    MainExtended.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,3 +6,5 @@ public class HelloWorld {
         System.out.println("F");
     }
 }
+
+
---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String consoleName = "Xbox One";
+        int quantity = 1;
+        double yearsInInsiderProgram = 5.8;
+        boolean happyUser = true;
+        char xboxVersion = 'f';
+        String userGameTag = "JanFedorowicz";
+
+        System.out.println(consoleName);
+        System.out.println(quantity);
+        System.out.println(yearsInInsiderProgram);
+        System.out.println(happyUser);
+        System.out.println(xboxVersion);
+        System.out.println(userGameTag);
+    }
+}
