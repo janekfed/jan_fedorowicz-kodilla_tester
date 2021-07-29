700ace847f88c736ecc7f170427de28cd32db88c
Jan Fedorowicz <janek.fedorowicz@gmail.com>
2021-07-29 19:10:29 +0200

    LeapYear.java

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;
 public class LeapYear {
     public static void main(String[] args) {
-        int rokPrzestepny = 2012;
-        if (rokPrzestepny % 4 == 0 && rokPrzestepny % 100 == 0 && rokPrzestepny % 400 == 0) {
+        int rokPrzestepny = 2020;
+        if (rokPrzestepny % 4 == 0 || rokPrzestepny % 100 == 0 && rokPrzestepny % 400 == 0) {
             System.out.println("Rok przestępny");
         }
         else {
