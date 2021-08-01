c1b55218d1a91ac6cf81624ffa274140f646979e
Jan Fedorowicz <janek.fedorowicz@gmail.com>
2021-08-01 21:21:32 +0200

    LeapYear.java

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Wybierz rok (minus przed rokiem dla lat p.n.e.):");
+        int leapYear = sc.nextInt();
+
+        if (leapYear >= -238 && leapYear % 4 == 0 || leapYear % 100 == 0 && leapYear % 400 == 0) {
+            System.out.println("Rok przestępny");
+        }
+        if (leapYear < -238) {
+            System.out.println("Rok przestępny wprowadzono dopiero w 258 r.p.n.e. ");
+        }
+        else {
+            System.out.println("Rok nie jest przestępny");
+        }
+    }
+}
